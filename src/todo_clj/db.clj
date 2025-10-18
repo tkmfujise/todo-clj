d8b614eeab6f1331a1a8e12076aa06e433289940
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2025-10-18 19:30:59 +0900

    Fix db-spec

--- src/todo_clj/db.clj
+++ src/todo_clj/db.clj
@@ -1,13 +1,13 @@
 (ns todo-clj.db
   (:require [clojure.java.jdbc :as jdbc]
-            [clojure.edn :as edn]
             [environ.core :refer [env]]))
 
 
-(def db-spec (edn/read-string (:db env)))
+(def db-spec {
+  :classname "org.sqlite.JDBC"
+  :subname "database.db"
+  :subprotocol "sqlite"})
 
-
-(def db-spec "jdbc:sqlite:databse.db")
 
 (defn migrated? []
   (pos? (count (jdbc/query db-spec "select name from sqlite_master where type='table'"))))
